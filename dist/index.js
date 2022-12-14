ee4c034e518abba4568b24fcad6acf8719a77599
takeru.hashimoto <hstake25@gmail.com>
2022-12-14 23:31:56 +0900

    first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.exec = exports.PEN_JS = void 0;
+exports.PEN_JS = "PEN_JS";
+var exec = function () {
+    console.log(exports.PEN_JS);
+};
+exports.exec = exec;
---
+++ index.ts
@@ -0,0 +1,5 @@
+export const PEN_JS = "PEN_JS";
+
+export const exec = () => {
+  console.log(PEN_JS);
+};
